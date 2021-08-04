5ef17c691d7b5c08f603f44bedb2caadde7c61a3
Fludorf <rataandrei1@gmail.com>
2021-08-04 16:37:12 +0300

    Long time no see.

--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -1,23 +1,29 @@
-package com.example.foody
+package com.example.foody.ui.fragments.recipes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.foody.R
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 class RecipesFragment : Fragment() {
+    //https://api.spoonacular.com/recipes/complexSearch?number=1&apiKey=def03f97eb664436ac3a62c793e582a7&type=drink&diet=vegan&addRecipeInformation=true&fillingredients=true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_recipes, container, false)
 
+
+
+        view.shimmer_recycler_view.showShimmer()
         return view
     }
-
 }
---
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.foody.util
+
+class Constants {
+
+    companion object {
+
+        const val API_KEY = "def03f97eb664436ac3a62c793e582a7"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
