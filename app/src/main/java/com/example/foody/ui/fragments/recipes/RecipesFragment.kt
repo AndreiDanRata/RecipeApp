bf3f4128bac74048257d258a285d30af25f3e021
Fludorf <rataandrei1@gmail.com>
2021-08-17 17:12:45 +0300

    Semi funtional BottomSheet with working chips for mealType and DietType.(BottomSheet data is stored into DataStoreRepository.kt)

--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foody.viewmodels.MainViewModel
 import com.example.foody.R
@@ -53,6 +54,10 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
         readDatabase()
+
+        binding.recipesFab.setOnClickListener() {
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
 
 
         return binding.root
--- app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
@@ -1,25 +1,54 @@
 package com.example.foody.viewmodels
 
 import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foody.data.DataStoreRepository
+import com.example.foody.data.MealAndDietType
 import com.example.foody.util.Constants
 import com.example.foody.util.Constants.Companion.API_KEY
+import com.example.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.foody.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import com.example.foody.util.Constants.Companion.QUERRY_ADD_RECIPE_INFORMATION
 import com.example.foody.util.Constants.Companion.QUERRY_API_KEY
 import com.example.foody.util.Constants.Companion.QUERRY_DIET
 import com.example.foody.util.Constants.Companion.QUERRY_FILL_INGREDIENTS
 import com.example.foody.util.Constants.Companion.QUERRY_NUMBER
 import com.example.foody.util.Constants.Companion.QUERRY_TYPE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+class RecipesViewModel @ViewModelInject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+    ) : AndroidViewModel(application) {
 
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
-        queries[QUERRY_NUMBER] = "50"
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
+        queries[QUERRY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERRY_API_KEY] = API_KEY
-        queries[QUERRY_TYPE] = "snack"
-        queries[QUERRY_DIET] = "vegan"
+        queries[QUERRY_TYPE] = mealType
+        queries[QUERRY_DIET] = dietType
         queries[QUERRY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERRY_FILL_INGREDIENTS] = "true"
 
