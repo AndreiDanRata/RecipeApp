35470c8ef034555d53f9ac1db67e97e89b8c76ba
Fludorf <rataandrei1@gmail.com>
2021-09-01 20:48:08 +0300

    Ingredients list -  work in progress:
    intredients_row_layout
    fragment_ingredients.xml

--- app/src/main/java/com/example/foody/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/example/foody/util/RecipesDiffUtil.kt
@@ -3,9 +3,13 @@ package com.example.foody.util
 import androidx.recyclerview.widget.DiffUtil
 import com.example.foody.models.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+/**
+ * Used for checking if the app data needs to be updated in the case it not stored already
+ * */
+
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ): DiffUtil.Callback() {
     override fun getOldListSize(): Int {
         return oldList.size
