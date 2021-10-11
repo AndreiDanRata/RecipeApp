dab3176e22631c11df7e0d87b9174d1e8878a869
rataa <rataandrei1@gmail.com>
2021-10-11 23:29:07 +0100

    Final Changes + App Icon + Extras

--- app/src/main/java/com/example/foody/ui/fragments/foodtrivia/FoodTriviaFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/foodtrivia/FoodTriviaFragment.kt
@@ -1,11 +1,10 @@
 package com.example.foody.ui.fragments.foodtrivia
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -18,12 +17,15 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class FoodJokeFragment : Fragment() {
+class FoodTriviaFragment : Fragment() {
 
     private val mainViewModel by viewModels<MainViewModel>()
 
     private var _binding: FragmentFoodTriviaBinding? = null
     private val binding get() = _binding!!
+
+
+    private var foodTrivia = "No Food Trivia"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,11 +35,16 @@ class FoodJokeFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.mainViewModel = mainViewModel
 
+        setHasOptionsMenu(true)
+
         mainViewModel.getFoodTrivia(API_KEY)
         mainViewModel.foodTriviaResponse.observe(viewLifecycleOwner, { response ->
             when(response) {
                 is NetworkResult.Success -> {
                     binding.foodTriviaTextView.text = response.data?.text
+                    if(response.data != null){
+                        foodTrivia = response.data.text
+                    }
                 }
                 is NetworkResult.Error -> {
                     loadDataFromCache()
@@ -56,11 +63,28 @@ class FoodJokeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_trivia_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.share_food_trivia_menu) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT,foodTrivia)
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun loadDataFromCache(){
        lifecycleScope.launch{
            mainViewModel.readFoodTrivia.observe(viewLifecycleOwner, { database ->
-               if(database.isNotEmpty() && database != null) {
+               if(!database.isNullOrEmpty()) {
                    binding.foodTriviaTextView.text = database[0].foodTrivia.text
+                   foodTrivia = database[0].foodTrivia.text
                }
 
            })
